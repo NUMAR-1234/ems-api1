ea330fac5cc93395cad85169bda51c70b66d5185
NUMAR-1234 <113420565+NUMAR-1234@users.noreply.github.com>
2024-01-13 00:37:00 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -5,16 +5,19 @@ STORES_LIST=$4
 
 echo "all the store list: ${STORES_LIST}"
 
-IFS=' ' read -ra STORES_ARRAY <<< "${STORES_LIST}"
+# Remove square brackets and commas from STORES_LIST
+CLEANED_STORES_LIST=$(echo "${STORES_LIST}" | tr -d '[],')
+
+IFS=' ' read -ra STORES_ARRAY <<< "${CLEANED_STORES_LIST}"
 
 API_URL="https://edgeapi.radius.ai/edge"
 
 STORE_LIST=""
 # Loop through the specified stores and fetch the data for each
 for store in "${STORES_ARRAY[@]}"; do
-  echo mysdcshdc "$store"
+  echo -n "mysdcshdc ${store} "
   store_data=$(curl -s --request GET "${API_URL}/dependency?tenant_name=${TENANT_NAME}&store_name=${store}" -H "CF-Access-Client-ID: ${CF_ACCESS_CLIENT_ID}" -H "CF-Access-Client-Secret: ${CF_ACCESS_CLIENT_SECRET}" | jq -rc '.[]')
-  STORE_LIST="${STORE_LIST}${store_data}"  # Concatenate the data for each store
+  STORE_LIST="${STORE_LIST}mysdcshdc ${store} "  # Concatenate the data for each store
 done
 
-
+echo "${STORE_LIST}"
