97593489577c10126efb1e4530c30db1778f9609
NUMAR-1234 <113420565+NUMAR-1234@users.noreply.github.com>
2024-01-12 23:52:17 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -6,3 +6,7 @@ STORES_LIST=$4  # Added the new input for the list of stores
 echo "all the store list: ${STORES_LIST}"
 IFS=' ' read -ra STORES_ARRAY <<< "${STORES_LIST}"
 
+echo "Updated format of store list:"
+for store in "${STORES_ARRAY[@]}"; do
+    echo "$store"
+done
