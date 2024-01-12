4e264c56864ba24827a4e05579fe13d6e179f323
NUMAR-1234 <113420565+NUMAR-1234@users.noreply.github.com>
2024-01-13 00:31:09 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -5,15 +5,16 @@ STORES_LIST=$4
 
 echo "all the store list: ${STORES_LIST}"
 
-# Remove square brackets and quotes from the list
-STORES_LIST=${STORES_LIST//[\[\]\"\'']}
+IFS=' ' read -ra STORES_ARRAY <<< "${STORES_LIST}"
 
-# Split the list into an array
-IFS=', ' read -ra STORES_ARRAY <<< "${STORES_LIST}"
+API_URL="https://edgeapi.radius.ai/edge"
 
-echo "Updated format of store list:"
-
-# Iterate through the array and print each store
+STORE_LIST=""
+# Loop through the specified stores and fetch the data for each
 for store in "${STORES_ARRAY[@]}"; do
-    echo "$store"
+  echo "$store"
+  store_data=$(curl -s --request GET "${API_URL}/dependency?tenant_name=${TENANT_NAME}&store_name=${store}" -H "CF-Access-Client-ID: ${CF_ACCESS_CLIENT_ID}" -H "CF-Access-Client-Secret: ${CF_ACCESS_CLIENT_SECRET}" | jq -rc '.[]')
+  STORE_LIST="${STORE_LIST}${store_data}"  # Concatenate the data for each store
 done
+
+
