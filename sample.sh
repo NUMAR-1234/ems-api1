277b7b73d96f11320b4ca788d9cb8961a7f771f8
NUMAR-1234 <113420565+NUMAR-1234@users.noreply.github.com>
2024-01-12 23:58:51 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,12 +1,20 @@
 ARTIFACT_NAME=$1
 TENANT_NAME=$2
 ARTIFACT_VERSION=$3
-STORES_LIST=$4  # Added the new input for the list of stores
+STORES_LIST=$4
 
 echo "all the store list: ${STORES_LIST}"
-IFS=' ' read -ra STORES_ARRAY <<< "${STORES_LIST}"
+
+# Remove square brackets and quotes from the list
+STORES_LIST=${STORES_LIST//[\"[\']/}
+STORES_LIST=${STORES_LIST//[\]\']/}
+
+# Split the list into an array
+IFS=', ' read -ra STORES_ARRAY <<< "${STORES_LIST}"
 
 echo "Updated format of store list:"
+
+# Iterate through the array and print each store
 for store in "${STORES_ARRAY[@]}"; do
     echo "$store"
 done
