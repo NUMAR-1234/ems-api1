7a50a4cc5905cc1316bfbc067db63fb062032965
NUMAR-1234 <113420565+NUMAR-1234@users.noreply.github.com>
2024-01-13 04:42:53 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -14,7 +14,6 @@ echo "$names_to_filter"
 json_data=$(cat data.json)
 
 # Convert command-line arguments to a JSON array of strings
-names_to_filter=($(printf '%s\n' "$@" | tr '\n' ',' | sed 's/,$//'))
 
 # Use jq to filter data based on the provided names
 filtered_data=$(echo "$json_data" | jq --arg names_to_filter "$names_to_filter" '.[] | select(.server_name as $item | ($names_to_filter | split(",") | index($item)) // empty)')
