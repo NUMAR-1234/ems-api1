fd2884734fe9a99db264e343f594508acad23669
NUMAR-1234 <113420565+NUMAR-1234@users.noreply.github.com>
2024-01-12 23:45:35 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -3,6 +3,6 @@ TENANT_NAME=$2
 ARTIFACT_VERSION=$3
 STORES_LIST=$4  # Added the new input for the list of stores
 
-echo "${STORES_LIST}"
+echo "all the store list: ${STORES_LIST}"
 IFS=' ' read -ra STORES_ARRAY <<< "${STORES_LIST}"
 
