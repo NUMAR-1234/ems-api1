b2a55968a7d84b4963f730a3db406193e25aa681
NUMAR-1234 <113420565+NUMAR-1234@users.noreply.github.com>
2024-01-13 04:23:31 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,23 +1,11 @@
-ARTIFACT_NAME=$1
-TENANT_NAME=$2
-ARTIFACT_VERSION=$3
-STORES_LIST=$4
+#!/bin/bash
 
-echo "all the store list: ${STORES_LIST}"
+json_data=$(cat data.json)
 
-# Remove square brackets and commas from STORES_LIST
-CLEANED_STORES_LIST=$(echo "${STORES_LIST}" | tr -d '[],')
+# Convert command-line arguments to a JSON array of strings
+names_to_filter=($(printf '%s\n' "$@" | tr '\n' ',' | sed 's/,$//'))
 
-IFS=' ' read -ra STORES_ARRAY <<< "${CLEANED_STORES_LIST}"
+# Use jq to filter data based on the provided names
+filtered_data=$(echo "$json_data" | jq --arg names_to_filter "$names_to_filter" '.[] | select(.server_name as $item | ($names_to_filter | split(",") | index($item)) // empty)')
 
-API_URL="https://edgeapi.radius.ai/edge"
-
-STORE_LIST=""
-# Loop through the specified stores and fetch the data for each
-for store in "${STORES_ARRAY[@]}"; do
-  echo -n "mysdcshdc ${store} "
-  store_data=$(curl -s --request GET "${API_URL}/dependency?tenant_name=${TENANT_NAME}&store_name=${store}" -H "CF-Access-Client-ID: ${CF_ACCESS_CLIENT_ID}" -H "CF-Access-Client-Secret: ${CF_ACCESS_CLIENT_SECRET}" | jq -rc '.[]')
-  STORE_LIST="${STORE_LIST}mysdcshdc ${store} "  # Concatenate the data for each store
-done
-
-echo "sample : ${STORE_LIST}"
+echo "$filtered_data"
